370ed016748885e77cbf2e9619d22a2fa17f2458
Moarcy <moarcy2@gmail.com>
2026-01-13 11:20:40 -0300

    atualização na geração de senha

--- src/main/java/bjjapp/service/UserService.java
+++ src/main/java/bjjapp/service/UserService.java
@@ -16,6 +16,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import bjjapp.controller.UserCreationResponse;
 import java.security.SecureRandom;
 import java.util.HashSet;
 import java.util.List;
